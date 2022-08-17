7e5ef22a031145aa789e2e899195f6762a3be66b
César León Schaus <cschaus@agropago.com>
2022-08-17 13:39:02 -0300

    [add] Initial commit

---
+++ test.ts
@@ -0,0 +1,16 @@
+const suma = (a: number, b: number) => {
+  return a + b;
+}
+
+const resta = (a: number, b: number) => {
+  return a - b;
+}
+const multiplica = (a: number, b: number) => {
+  return a * b;
+}
+const divide = (a: number, b: number) => {
+  if(b>0){
+    return a / b;
+  }
+  return;
+}
