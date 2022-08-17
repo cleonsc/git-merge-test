9fadf3c387e3f616269e95b8a0530e414defd861
César León Schaus <cschaus@agropago.com>
2022-08-17 13:43:58 -0300

    [edit] only suma and multiplica

--- test.ts
+++ test.ts
@@ -2,15 +2,6 @@ const suma = (a: number, b: number) => {
   return a + b;
 }
 
-const resta = (a: number, b: number) => {
-  return a - b;
-}
 const multiplica = (a: number, b: number) => {
   return a * b;
-}
-const divide = (a: number, b: number) => {
-  if(b>0){
-    return a / b;
-  }
-  return;
 }
